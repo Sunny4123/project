597b96a27088e90fe8975d8a34ef2e8060d16f7f
662110187 <narongthon_s@cmu.ac.th>
2024-01-27 18:38:11 +0700

    c

--- con_order.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include_once "sever.php";
-    $id = $_GET['id'];
-    $sql = "DELETE FROM myorder WHERE id = '$id'";
-    $result = mysqli_query($connect,$sql);
-    header('location: my_order.php');
-?>
--- script2.js
+++
@@ -1,6 +0,0 @@
-Swal.fire({
-    icon: 'success',
-    title: 'successful Login',
-    showConfirmButton: false,
-    timer: 1500
-});
